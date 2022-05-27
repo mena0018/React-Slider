f273d31e9568349527ecdcf21edc701dc0cd54d3
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-05-28 00:10:01 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './Components/Home';
+import './index.scss';
+
+function App() {
+  return (
+
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.js
@@ -0,0 +1,11 @@
+import "./Home.scss"
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        Home
+      </div>
+    </>
+  )
+}
